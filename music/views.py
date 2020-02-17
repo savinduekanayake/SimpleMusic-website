03af569a82e3dd60d6c0a0a21f5527285ca2098f
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-02-18 00:09:28 +0530

    display albums

--- music/views.py
+++ music/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1> This is the music app Home Page<h1>")
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request,album_id):
     return HttpResponse("<h2>Details for album id: "+ str(album_id)+ "</h2>")
