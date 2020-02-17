9701594410c710d9dabf68ac9a47afba01cd1fa8
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-02-18 04:45:38 +0530

    added add to favorite button

--- music/urls.py
+++ music/urls.py
@@ -2,11 +2,16 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     url('^$', views.index, name='index'),
 
-    # /music/71/
+    # /music/<album_id>/
     url(r'^(?P<album_id>[[0-9]+)/$',views.detail, name='detail'),
+
+        # /music/<album_id>/favorite/
+    url(r'^(?P<album_id>[[0-9]+)/favorite/$',views.favorite, name='favorite'),
 
 ]
