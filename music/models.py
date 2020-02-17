4351eec8434a26ba332162b159e8ffef61a4050d
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-02-18 02:24:39 +0530

    adding songs to database and display in the page

--- music/models.py
+++ music/models.py
@@ -17,4 +17,7 @@ class Song(models.Model):
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.song_title
+
 
