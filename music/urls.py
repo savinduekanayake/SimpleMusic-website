0229eb594028957fcafb6b5b314f03a006d331f3
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-02-19 01:45:38 +0530

    fixed errors, upload photoes

--- music/urls.py
+++ music/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(), name='detail'),
 
     #/music/album/add/
-    url('album/add/$',views.AlbumUpdate.as_view(), name='album-add'),
+    url('album/add/$',views.AlbumCreate.as_view(), name='album-add'),
 
     #/music/album/2/
-    url('album/(?P<pk>[0-9]+)/$',views.AlbumCreate.as_view(), name='album-update'),
+    url('album/(?P<pk>[0-9]+)/$',views.AlbumUpdate.as_view(), name='album-update'),
 
     #/music/album/2/delete/
     url('album/(?P<pk>[0-9]+)/delete/$',views.AlbumDelete.as_view(), name='album-delete'),
--- website/urls.py
+++ website/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('music/',include('music.urls')),
@@ -28,5 +29,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
